5e929134d8d8afc089764d584404ce494dac7469
Avnish Anand <62175110+anand-avnish@users.noreply.github.com>
2023-01-30 12:22:03 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      'raleway':['Raleway', 'sans-serif'],
+      'poppins': ['Poppins', 'sans-serif'],
+      'courgette':['Courgette', 'cursive'],
+      'montserrat':['Montserrat', 'sans-serif'],
+      'inter':['Inter', 'sans-serif']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
